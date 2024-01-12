67660dd5e5960dc65aa56830d4032b9cbaf1aa93
Sithara Azeez <sitharasithara2@gmail.com>
2024-01-13 01:42:40 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import NavBar from './Components/NavBar/NavBar';
+import {originals, action, comedy, romance} from './urls'
+import Banner from './Components/Banner/Banner';
+import RowPost from './Components/RowPost/RowPost';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>sithara</h1>
+      < NavBar />
+      <Banner />
+      <RowPost url={originals} title='Netflix Originals' />
+      <RowPost url={action} title='Action' isSmall />
+      <RowPost url={comedy} title='Comedy Movies' />
+      <RowPost url={romance} title='Romance' isSmall />
+      
     </div>
   );
 }
---
+++ src/constants/constants.js
@@ -0,0 +1,3 @@
+export const baseUrl = 'https://api.themoviedb.org/3';
+export const API_KEY = '1973bb8809ec51adea34e75257bbf090';
+export const imageUrl = 'https://image.tmdb.org/t/p/original'
